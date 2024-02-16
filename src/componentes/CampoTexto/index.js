8e7f6506bdd316cfcd652cdd59c23f4263b26ac1
Fellipe Juncal <fjuncal7@gmail.com>
2024-02-16 18:46:35 -0300

    Criando props do formulario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,9 +1,10 @@
 import './CampoTexto.css'
-const CampoTexto = () => {
+const CampoTexto = (props) => {
+    const placeHolderModificada = `${props.placeholder}...`
     return (
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder='Digite o seu nome'/>
+            <label>{props.label}</label>
+            <input placeholder={placeHolderModificada}/>
         </div>
     )
 }
