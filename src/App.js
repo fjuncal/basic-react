efb6a395b37cfd81249f69dcf3783c9535debece
Fellipe Juncal <fjuncal7@gmail.com>
2024-02-16 20:43:45 -0300

    Criando componente de time

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
-    console.log(colaboradores)
   }
 
   return (
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Front-End"/>
+      <Time nome="Data Science"/>
     </div>
   );
 }
