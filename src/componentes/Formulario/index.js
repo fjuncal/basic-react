d54f6d21285cac57416c6f06f9c8ece536d01ed5
Fellipe Juncal <fjuncal7@gmail.com>
2024-02-19 15:39:46 -0300

    Elevando o nivel de estado de times para o arquivo app.js

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,15 +5,6 @@ import Botao from '../Botao';
 import { useState } from 'react';
 
 const Formulario = (props) => {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data-Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -55,7 +46,7 @@ const Formulario = (props) => {
                 <Dropdown 
                     obrigatorio={true} 
                     label="Time" 
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
