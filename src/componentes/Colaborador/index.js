6d76479c5448f0f455e4d7eb56c6a36aff63d467
Fellipe Juncal <fjuncal7@gmail.com>
2024-02-19 15:45:03 -0300

    Criando o componente de card de forma estática

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div>
+            <div>
+                <img src='https://github.com/fjuncal.png' alt='mine foto'/>
+            </div>
+            <div>
+                <h4>Fellipe Juncal</h4>
+                <h5>Aluno</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
